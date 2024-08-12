d44179fccaeaedfcb5461e0cf48c468e2768a8da
Maiten Blanc <maitengblanc@gmail.com>
2024-08-12 18:01:01 -0300

    Front terminado

--- form.js
+++ form.js
@@ -7,24 +7,50 @@ function altoContraste() {
 }
 
 const fechaNacimiento = document.getElementById("date");
-fechaNacimiento.addEventListener("change", (evento) => {
-    verificarFecha(evento.target);
-});
+const edadError = document.getElementById("error");
 
 function verificarFecha() {
     let fecha = new Date(fechaNacimiento.value);
     let fechaActual = new Date();
     let edad = fechaActual.getFullYear() - fecha.getFullYear();
+    let mes = fechaActual.getMonth() - fecha.getMonth();
 
-    const edadError = document.querySelector(".error");
+    if (mes < 0 || (mes === 0 && fechaActual.getDate() < fecha.getDate())) {
+        edad--;
+    }
 
-    fechaNacimiento.addEventListener("input", (event) => {
-        if (edad >= 18) {
-            edadError.innerHTML = "Debes ser mayor de 18 años.";
-            edadError.className = "error";
-        } else {
-            edadError.innerHTML = "";
-        }return 
-    })
-    
+    if (edad < 18) {
+        edadError.innerHTML = "Debes ser mayor de 18 años.";
+        edadError.className = "error";
+        return false;
+    } else {
+        edadError.innerHTML = "";
+        return true;
+    }
 }
+fechaNacimiento.addEventListener("input", verificarFecha);
+
+
+function validarBlancos() {
+    let nombre = document.getElementById("nombre").value.trim();
+    let apellido = document.getElementById("apellido").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let pais = document.getElementById("pais").value.trim();
+
+    if (nombre === "" || apellido === "" || email === "" || pais === "") {
+        alert("Ningún campo puede estar vacío ni contener solo espacios en blanco.");
+        return false;
+    }
+    return true;
+}
+
+document.getElementById("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const esFechaValida = verificarFecha();
+    const sonCamposValidos = validarBlancos();
+
+    if (esFechaValida && sonCamposValidos) {
+        window.location.href = "./form_enviado/form_enviado.html";
+    }
+
+});
